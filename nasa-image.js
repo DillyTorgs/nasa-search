4eb968fecf9da6153bcbc23c886f0ba0b3a285a5
DillyTorgs <Djt5832@psu.edu>
2024-10-27 13:25:28 -0400

    first

---
+++ nasa-image.js
@@ -0,0 +1,93 @@
+import { LitElement, html, css } from "lit";
+
+
+export class NasaImage extends LitElement {
+
+  constructor() {
+    super();
+    this.title = 'Untitled';
+    this.source = '';
+    this.alt ='';
+    this.owner = 'Unknown Owner';
+  }
+
+  static get properties() {
+    return {
+        source: { type: String },
+        title: { type: String },
+        alt: { type: String },
+        owner: { type: String }
+    };
+  }
+
+  static get styles() {
+    return [css`
+    
+
+    .image {
+      
+      width: var(--ddd-font-size-type1-m);
+      height: var(--ddd-font-size-type1-m);
+      padding-left: var(--ddd-spacing-4);
+      margin-left:var(--ddd-spacing-7);
+      margin-top: var(--ddd-spacing-2);
+      
+        
+
+    }
+
+    .card {
+      display: inline-block;
+      width: 240px;
+      height: 240px;
+      border: var(--ddd-border-sm);
+      border-color: var(--ddd-theme-default-nittanyNavy);
+      box-shadow: var(--ddd-boxShadow-xl);
+      border-radius: var(--ddd-spacing-3);
+      text-decoration: none;
+      margin-bottom: var(--ddd-spacing-2);
+      margin-top: var(--ddd-spacing-2);
+      background-color: var(--ddd-theme-default-limestoneLight);
+    }
+    .card:hover{
+      background-color: var(--ddd-theme-default-keystoneYellow);
+    }
+
+    .card:focus{
+      background-color: var(--ddd-theme-default-opportunityGreen);
+      outline: none;
+    }
+
+    .title, .owner
+    {
+      text-decoration: none;
+      text-align: center;
+      font-size: var(--ddd-spacing-3);
+      color: var(--ddd-theme-default-beaverBlue);
+
+    }
+
+    .title{
+      font-weight: bold;
+    }
+
+    
+    `];
+  }
+
+  render() {
+    return html`
+    <div class="card" tabindex="0">
+        <img class="image" src="${this.source}" alt="${this.alt}"/>
+        <br>
+        <div class="title">${this.title}</div>
+        <div class="owner">${this.owner}</div>
+    
+    </div>
+    `;
+  }
+  static get tag() {
+    return "nasa-image";
+  }
+}
+customElements.define(NasaImage.tag, NasaImage);
--- nasa-search.js
+++ nasa-search.js
@@ -1,84 +1,142 @@
-/**
- * Copyright 2024 DillyTorgs
- * @license Apache-2.0, see LICENSE for full text.
- */
-import { LitElement, html, css } from "lit";
-import { DDDSuper } from "@haxtheweb/d-d-d/d-d-d.js";
-import { I18NMixin } from "@haxtheweb/i18n-manager/lib/I18NMixin.js";
+import { LitElement, html, css } from 'lit';
+import { DDDSuper } from "@lrnwebcomponents/d-d-d/d-d-d.js";
 
-/**
- * `nasa-search`
- * 
- * @demo index.html
- * @element nasa-search
- */
-export class nasaSearch extends DDDSuper(I18NMixin(LitElement)) {
 
-  static get tag() {
-    return "nasa-search";
+import "./nasa-image.js";
+export class NasaSearch extends LitElement {
+  static get properties() {
+    return {
+      title: { type: String },
+      loading: { type: Boolean, reflect: true },
+      items: { type: Array, },
+      value: { type: String },
+    };
+  }
+
+  static get styles() {
+    return css`
+      :host {
+        display: block;
+        
+      }
+      :host([loading]) .results {
+        opacity: 0.1;
+        visibility: hidden;
+        height: var(--ddd-ls-72-lg);
+      }
+      .results {
+        visibility: visible;
+        height: 100%;
+        opacity: 1;
+        transition-delay: .5s;
+        transition: .5s all ease-in-out;
+        display:flex;
+        flex-wrap: wrap;
+        justify-content: space-evenly;
+       
+        
+
+      }
+
+      details {
+        margin: var(--ddd-spacing-4);
+        padding: var(--ddd-spacing-4);
+        background-color: var(--ddd-theme-default-nittanyNavy);
+        border-radius: var(--ddd-spacing-2);
+      }
+      summary {
+        font-size: var(--ddd-spacing-6);
+        padding: var(--ddd-spacing-2);
+        color: var(--ddd-theme-default-pughBlue);
+        font-size: var(--ddd-spacing-10);
+        text-align: center;
+        font-family: var(--ddd-font-secondary);
+        
+      }
+      input {
+        font-size: var( --ddd-spacing-5);
+        line-height: var(--ddd-spacing-10);
+        width: 100%;
+        border-radius: var(--ddd-spacing-2);
+        padding-left: var(--ddd-spacing-2);
+        
+      }
+
+      
+    `;
   }
 
   constructor() {
     super();
-    this.title = "";
-    this.t = this.t || {};
-    this.t = {
-      ...this.t,
-      title: "Title",
-    };
-    this.registerLocalization({
-      context: this,
-      localesPath:
-        new URL("./locales/nasa-search.ar.json", import.meta.url).href +
-        "/../",
-      locales: ["ar", "es", "hi", "zh"],
+    this.value = null;
+    this.title = '';
+    this.loading = false;
+    this.items = [];
+  }
+
+  render() {
+    return html`
+    <h2>${this.title}</h2>
+
+    <details open>
+      <summary>NASA Images</summary>
+      <div>
+        <input id="input" placeholder="Space and Stuff" @input="${this.inputChanged}" />
+      </div>
+    </details>
+    
+    <div class="results">
+      
+  
+    
+      ${this.items.map((item, index) => html`
+        <a href="${item.links[0].href}" target="_blank" tabindex="0">
+        
+
+      <nasa-image
+        source="${item.links[0].href}"
+        title="${item.data[0].title}"
+        alt="${item.data[0].description}"
+        owner="${item.data[0].secondary_creator}"
+
+      ></nasa-image>
+      </a>
+      `)}
+    </div>
+    `;
+  }
+
+  inputChanged(e) {
+    this.value = this.shadowRoot.querySelector('#input').value;
+  }
+  // life cycle will run when anything defined in `properties` is modified
+  updated(changedProperties) {
+    // see if value changes from user input and is not empty
+    if (changedProperties.has('value') && this.value) {
+      this.updateResults(this.value);
+    }
+    else if (changedProperties.has('value') && !this.value) {
+      this.items = [];
+    }
+    // @debugging purposes only
+    if (changedProperties.has('items') && this.items.length > 0) {
+      console.log(this.items);
+    }
+  }
+
+  updateResults(value) {
+    this.loading = true;
+    fetch(`https://images-api.nasa.gov/search?media_type=image&q=${value}`).then(d => d.ok ? d.json(): {}).then(data => {
+      if (data.collection) {
+        this.items = [];
+        this.items = data.collection.items;
+        this.loading = false;
+      }  
     });
   }
 
-  // Lit reactive properties
-  static get properties() {
-    return {
-      ...super.properties,
-      title: { type: String },
-    };
-  }
-
-  // Lit scoped styles
-  static get styles() {
-    return [super.styles,
-    css`
-      :host {
-        display: block;
-        color: var(--ddd-theme-primary);
-        background-color: var(--ddd-theme-accent);
-        font-family: var(--ddd-font-navigation);
-      }
-      .wrapper {
-        margin: var(--ddd-spacing-2);
-        padding: var(--ddd-spacing-4);
-      }
-      h3 span {
-        font-size: var(--nasa-search-label-font-size, var(--ddd-font-size-s));
-      }
-    `];
-  }
-
-  // Lit render the HTML
-  render() {
-    return html`
-<div class="wrapper">
-  <h3><span>${this.t.title}:</span> ${this.title}</h3>
-  <slot></slot>
-</div>`;
-  }
-
-  /**
-   * haxProperties integration via file reference
-   */
-  static get haxProperties() {
-    return new URL(`./lib/${this.tag}.haxProperties.json`, import.meta.url)
-      .href;
+  static get tag() {
+    return 'nasa-search';
   }
 }
-
-globalThis.customElements.define(nasaSearch.tag, nasaSearch);
+customElements.define(NasaSearch.tag, NasaSearch);
